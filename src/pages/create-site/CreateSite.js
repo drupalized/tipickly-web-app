07532bc053eb6ca5b173a415929367f41e2c54cc
Rafael Nogueira <rafael_nogueira@epam.com>
2019-05-10 18:09:11 -0500

    Adding the Create Site form

---
+++ src/pages/create-site/CreateSite.js
@@ -0,0 +1,17 @@
+// Global components.
+import React, { Component } from 'react';
+
+// Styles.
+import '../../styles/pages/create-site/CreateSite.scss';
+
+class CreateSite extends Component {
+  render() {
+    return (
+      <div className="Create-site">
+
+      </div>
+    );
+  }
+}
+
+export default CreateSite;
